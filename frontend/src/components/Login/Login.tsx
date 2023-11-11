76144bd1d15be76d5a4f0e2bb003db8e706b3c28
Kaua <kauaduarte@gmail.com>
2023-11-11 13:57:56 -0300

    Update Login.tsx

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -21,7 +21,7 @@ export function Login() {
         };
 
         try{
-            const response = await axios.get(
+            const response = await axios.post(
                 'http://127.0.0.1:8000/api/login',
                 {
                     email: login.email,
