ec9039014518212e264ec4ce528759d65fe54ebc
andreneery <neryandre8@gmail.com>
2023-11-16 20:06:23 -0300

    ajustando profile

--- frontend/src/components/Profile/Profile.tsx
+++ frontend/src/components/Profile/Profile.tsx
@@ -90,137 +90,56 @@ export function Profile() {
                     <b style={{ fontSize: '1.25rem' }}>Publicações</b>
                 </div>
 
-            <div style={{ margin: '1rem' }}>
-                <div className='d-flex mt-2'>
-                    <a href="#" className="nav-link d-flex flex-row mt-4">
-                    <img
-                        src="https://avatars.githubusercontent.com/u/88936386?v=4"
-                        alt="Imagem do perfil"
-                        className="img-fluid rounded-circle align-self-start"
-                        style={{ maxWidth: "40px"}}
-                    />
-                    <div className="mt-2" style={{marginLeft: '1rem'}}>
-                        <div>
-                        <span>Alex Sander Meneses Santos</span>
-                        <span> . </span>
-                        <span style={{ color: '#5b6b77' }}>15h</span>
-                        </div>
-                    </div>
-                    </a>
-                </div>
-
-                <div style={{ padding: '0 3rem' }}>
-                    <span className='text-justify' style={{ fontSize: '0.85rem' }}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquet semper felis, 
-                    at dapibus augue efficitur ut. Cras tempor id lacus in interdum. 
-                    Mauris quis nunc vestibulum purus rutrum tempor vitae a metus. 
-                    Cras eleifend vehicula justo ac mollis. Mauris eros elit, posuere ut odio ut, cursus da
-                    </span>
-                    <img 
-                    src="https://i.pinimg.com/564x/f2/2f/86/f22f86b18331d9712e90c7db064ea33c.jpg"
-                    alt="Imagem do perfil"
-                    className="img-fluid align-self-start mt-3"
-                    style={{ width: "600px", height: '300px', borderRadius: '10px', objectFit: 'cover'}}
-                    />
-                    <div style={{ display: 'flex', alignItems: 'center', marginTop: '10px', color: '#5b6b77' }}>
-                    <span className="material-symbols-outlined">favorite</span>
-                    <span style={{ marginLeft: '0.25rem' }}>12</span>
-
-                    <span className="material-symbols-outlined" style={{ marginLeft: '2rem' }}>forum</span>
-                    <span style={{ marginLeft: '0.25rem' }}>20</span>
-                    </div>
-                </div>
-            </div>
-
             <hr style={{ borderTop: '1px solid gray', marginTop: '2rem' }} />
 
-            <div style={{ margin: '1rem' }}>
-                <div className='d-flex'>
-                    <a href="#" className="nav-link d-flex flex-row mt-4">
-                    <img
-                        src="https://avatars.githubusercontent.com/u/88936386?v=4"
-                        alt="Imagem do perfil"
-                        className="img-fluid rounded-circle align-self-start"
-                        style={{ maxWidth: "40px"}}
-                    />
-                    <div className="mt-2" style={{marginLeft: '1rem'}}>
-                        <div>
-                        <span>Kauã Duarte</span>
-                        <span> . </span>
-                        <span style={{ color: '#5b6b77' }}>15h</span>
-                        </div>
-                    </div>
-                    </a>
-                </div>
-
-                <div style={{ padding: '0 3rem' }}>
-                    <span className='text-justify' style={{ fontSize: '0.85rem' }}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquet semper felis, 
-                    at dapibus augue efficitur ut. Cras tempor id lacus in interdum. 
-                    Mauris quis nunc vestibulum purus rutrum tempor vitae a metus. 
-                    Cras eleifend vehicula justo ac mollis. Mauris eros elit, posuere ut odio ut, cursus da
-                    </span>
-                    <img 
-                    src="https://i.pinimg.com/564x/4e/62/78/4e627880655e0f29e4c1cb428db6b089.jpg"
+            {loadingPostagens ? (
+          <p>Carregando postagens...</p>
+        ) : (
+          postagens.map((post) => (
+            <div style={{ margin: '1rem' }} key={post.id}>
+              <div className="d-flex mt-2">
+                <a href="#" className="nav-link d-flex flex-row mt-4">
+                  <img
+                    src={`http://127.0.0.1:8000/api/user/getImage/${post.user_id}`}
                     alt="Imagem do perfil"
-                    className="img-fluid align-self-start mt-3"
-                    style={{ width: "600px", height: '300px', borderRadius: '10px', objectFit: 'cover'}}
-                    />
-                    <div style={{ display: 'flex', alignItems: 'center', marginTop: '10px', color: '#5b6b77' }}>
-                    <span className="material-symbols-outlined">favorite</span>
-                    <span style={{ marginLeft: '0.25rem' }}>12</span>
-
-                    <span className="material-symbols-outlined" style={{ marginLeft: '2rem' }}>forum</span>
-                    <span style={{ marginLeft: '0.25rem' }}>20</span>
+                    className="img-fluid rounded-circle align-self-start"
+                    style={{ maxWidth: '40px' }}
+                  />
+                  <div className="mt-2" style={{ marginLeft: '1rem' }}>
+                    <div>
+                      <span>
+                        {post.name} {post.last_name}
+                      </span>
                     </div>
+                  </div>
+                </a>
+              </div>
+
+              <div style={{ padding: '0 3rem' }}>
+                <span className="text-justify" style={{ fontSize: '0.85rem', marginLeft: '0.5rem' }}>
+                  {post.content}
+                </span>
+                <div
+                  style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    marginTop: '10px',
+                    color: '#5b6b77',
+                  }}
+                >
+                  <span className="material-symbols-outlined">favorite</span>
+                  <span style={{ marginLeft: '0.25rem' }}>12</span>
+
+                  <span className="material-symbols-outlined" style={{ marginLeft: '2rem' }}>
+                    forum
+                  </span>
+                  <span style={{ marginLeft: '0.25rem' }}>20</span>
                 </div>
+              </div>
+              <hr style={{ borderTop: '1px solid gray', marginTop: '2rem' }} />
             </div>
-
-                <hr style={{ borderTop: '1px solid gray', marginTop: '2rem' }} />
-
-            <div style={{ margin: '1rem' }}>
-                <div className='d-flex'>
-                    <a href="#" className="nav-link d-flex flex-row mt-4">
-                    <img
-                        src="https://avatars.githubusercontent.com/u/88936386?v=4"
-                        alt="Imagem do perfil"
-                        className="img-fluid rounded-circle align-self-start"
-                        style={{ maxWidth: "40px"}}
-                    />
-                    <div className="mt-2" style={{marginLeft: '1rem'}}>
-                        <div>
-                        <span>Kauã Duarte</span>
-                        <span> . </span>
-                        <span style={{ color: '#5b6b77' }}>15h</span>
-                        </div>
-                    </div>
-                    </a>
-                </div>
-
-                <div style={{ padding: '0 3rem' }}>
-                    <span className='text-justify' style={{ fontSize: '0.85rem' }}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquet semper felis, 
-                    at dapibus augue efficitur ut. Cras tempor id lacus in interdum. 
-                    Mauris quis nunc vestibulum purus rutrum tempor vitae a metus. 
-                    Cras eleifend vehicula justo ac mollis. Mauris eros elit, posuere ut odio ut, cursus da
-                    </span>
-                    <img 
-                    src="https://i.pinimg.com/564x/82/cb/f0/82cbf0e0b4126418a8267f2880b3f286.jpg"
-                    alt="Imagem do perfil"
-                    className="img-fluid align-self-start mt-3"
-                    style={{ width: "600px", height: '300px', borderRadius: '10px', objectFit: 'cover'}}
-                    />
-                    <div style={{ display: 'flex', alignItems: 'center', marginTop: '10px', color: '#5b6b77' }}>
-                    <span className="material-symbols-outlined">favorite</span>
-                    <span style={{ marginLeft: '0.25rem' }}>12</span>
-
-                    <span className="material-symbols-outlined" style={{ marginLeft: '2rem' }}>forum</span>
-                    <span style={{ marginLeft: '0.25rem' }}>20</span>
-                    </div>
-                </div>
-            </div>
-
-
+          ))
+        )}
             </div>
             ))}
         </>
