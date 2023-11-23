8bcaa45d1339a5877aa581a799832a1b7e2d91df
ThalitaBispo <thallitaasoares@gmail.com>
2023-11-23 17:30:14 -0300

    nome do club

--- frontend/src/components/Dashboard/RoutesDashboard.tsx
+++ frontend/src/components/Dashboard/RoutesDashboard.tsx
@@ -17,7 +17,28 @@ import { Reunion } from '../Reunion/Reunion';
 import { CreateReunion } from '../Reunion/CreateReunion';
 import { EditReunion } from '../Reunion/EditReunion';
 
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import Cookies from 'js-cookie';
+
 export function RoutesDashboard() {
+
+    const [nameClub, setNameClub] = useState([]);
+    const club_id = Cookies.get('club_id');
+
+    useEffect(() => {
+        async function clubName() {
+            try {
+                const response = await axios.get(`http://localhost:8000/api/club/getClubById/${club_id}`);
+                setNameClub(response.data[0]);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+
+        clubName();
+    }, []);
+
     return(
         <>
             <BrowserRouter>
@@ -31,13 +52,13 @@ export function RoutesDashboard() {
                             <div className="col-md-5" style={{ maxHeight: '50%', overflowY: 'auto' }}>
                                 <div className="row">
                                     <div className="col-md-6 mt-4">
-                                        <b>Clube dos ++</b>
+                                        <b>{nameClub.name}</b>
                                     </div>
-                                    <div className={`col-md-6 mt-4 ${styles.menu}`}>
+                                    {/*<div className={`col-md-6 mt-4 ${styles.menu}`}>
                                         <a href="#" className="nav-link">
                                         <span className="material-symbols-outlined">notifications</span>
                                         </a>
-                                    </div>
+                                    </div>*/}
                                 </div>
                                 <hr style={{ borderTop: '2px solid gray' }} />
 
